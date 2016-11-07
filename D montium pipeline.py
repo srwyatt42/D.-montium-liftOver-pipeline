9d874aee65e24160efd232fe9ff17c0a98581d22
Sydney Wyatt <srwyatt42@gmail.com>
2016-11-06 19:16:43 -0800

    Python Script Outline Added

---
+++ D montium pipeline.py
@@ -0,0 +1,11 @@
+#Pipeline for D montium genomes, phylogenetic analysis
+
+#User input genome(s)
+
+#User input gene sequence(s)
+
+#Pipe to liftOver application to find loci in each genome
+
+#liftOver outputs chain files of loci in each genome
+
+#Convert files into FASTA format for use in other programs (alignment, etc)
