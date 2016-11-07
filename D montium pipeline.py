7fc6bbfecaeb8cd951f534f551292b754f66ee12
Sydney Wyatt <srwyatt42@gmail.com>
2016-11-07 14:11:32 -0800

    Pipeline Outline Updated

--- D montium pipeline.py
+++ D montium pipeline.py
@@ -4,8 +4,11 @@
 
 #User input gene sequence(s)
 
-#Pipe to liftOver application to find loci in each genome
+#Convert file to BED format
 
+#Pipe to liftOver application to find loci in each genome
+    #liftOver needs to be installed on user's computer
+    #Path will be specified here; include in README
 #liftOver outputs chain files of loci in each genome
 
 #Convert files into FASTA format for use in other programs (alignment, etc)
