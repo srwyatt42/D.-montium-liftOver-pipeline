e720cb84983eda85a3256cb611c5028c9178cde1
Sydney Wyatt <srwyatt42@gmail.com>
2016-11-07 16:53:17 -0800

    Update D montium pipeline.py

--- D montium pipeline.py
+++ D montium pipeline.py
@@ -1,9 +1,10 @@
 #Pipeline for D montium genomes, phylogenetic analysis
 
 #User input genome(s)
+genome = input("Enter genome file: ", "r+")
 
 #User input gene sequence(s)
-
+loci = input("Enter gene sequence file: ", "r+")
 #Convert file to BED format
 
 #Pipe to liftOver application to find loci in each genome
