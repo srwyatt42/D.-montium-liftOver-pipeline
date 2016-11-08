faffebc10239d643059224b0145d70f47356eba8
Sydney Wyatt <srwyatt42@gmail.com>
2016-11-07 17:11:53 -0800

    Add files via upload

--- D montium pipeline.py
+++ D montium pipeline.py
@@ -1,10 +1,35 @@
 #Pipeline for D montium genomes, phylogenetic analysis
 
+genomes = []    #stores genome sequence files
+loci = []       #stores loci sequence files
+
 #User input genome(s)
-genome = input("Enter genome file: ", "r+")
+genome = input("Enter genome file: ")
+genomes.append(genome)
+more_genomes = input("Add another? ")
+#to add multiple genome files
+if more_genomes == "y":
+    y = input("Enter genome file: ")
+    genomes.append(y)
+    more_genomes = input("Add another? ")
+if more_genomes == "n":
+    print("File(s) added")
+    print(genomes)
 
 #User input gene sequence(s)
-loci = input("Enter gene sequence file: ", "r+")
+locus = input("Enter gene sequence file: ")
+loci.append(locus)
+more_loci = input("Add another? ")
+#to add mutliple gene sequence files
+if more_loci == "y":
+    y = input("Enter genome file: ")
+    loci.append(y)
+    more_loci = input("Add another? ")
+if more_loci == "n":
+    print("File(s) added")
+    print(loci)
+
+
 #Convert file to BED format
 
 #Pipe to liftOver application to find loci in each genome
