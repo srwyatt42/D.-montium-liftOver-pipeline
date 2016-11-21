9eb201a4e00f78b85d2a0b390873005d441679ee
Sydney Wyatt <srwyatt42@gmail.com>
2016-11-21 14:35:39 -0800

    iterating over the list of files?

--- file-input-tests.py
+++ file-input-tests.py
@@ -15,3 +15,11 @@ import subprocess
 subprocess.call(['cd', '/Users/kopplab/Desktop/Syd_LiftOver_Stuff'])
 print(subprocess.check_output(['pwd']))
 #subprocess not changing the directory correctly
+
+for i in chain_files2:
+    print(i)
+    for i in bed_files:
+        print(i)
+        subprocess.call(['./liftover', '-minMatch=0.1', '-multiple', 'bed_files[i]', 'chain_files2[i]', 'output_file.bed', 'unMapped'])
+#fix the for loop so that it feeds into liftover correctly
+#is there a way to get output_file.bed to be filename.bed?
